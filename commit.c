bc05422eda000810f3aa487d3e4fdd0a3a017b99
DEV-2828 <devopampal@gmail.com>
2026-04-20 11:49:01 +0530

    feat(commit): implement commit tree creation

--- commit.c
+++ commit.c
@@ -194,8 +194,46 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    Commit c;
+    memset(&c, 0, sizeof(Commit));
+
+    if (tree_from_index(&c.tree) != 0) {
+        return -1;
+    }
+
+    ObjectID parent_id;
+    if (head_read(&parent_id) == 0) {
+        c.parent = parent_id;
+        c.has_parent = 1;
+    } else {
+        c.has_parent = 0;
+    }
+
+    const char *author = pes_author();
+    strncpy(c.author, author, sizeof(c.author) - 1);
+    c.author[sizeof(c.author) - 1] = '\0';
+    
+    c.timestamp = (uint64_t)time(NULL);
+    
+    strncpy(c.message, message, sizeof(c.message) - 1);
+    c.message[sizeof(c.message) - 1] = '\0';
+
+    void *data;
+    size_t len;
+    if (commit_serialize(&c, &data, &len) != 0) {
+        return -1;
+    }
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
+    if (head_update(commit_id_out) != 0) {
+        return -1;
+    }
+
+    return 0;
 }
